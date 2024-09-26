cae90afa061446d936a43dabb8b73a29ff9f97fa
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2024-09-26 06:53:00 -0700

    Refactor ImageSlider component to update post item layout in +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -46,10 +46,12 @@
 				<li class="post" style={`--transition-name: post-${post.slug}`}>
 					<Side_By_Side>
 						<Image src={post.image} alt={post.title} />
-						<a href={`/blog/${post.slug}`} class="title">
+						< href={`/blog/${post.slug}`} class="title">
 							<h2>{post.title}</h2>
-							<p class="date">{formatDate(post.date)}</p>
-							<p class="description">{post.description}</p>
+							<div class="meta">
+								<p class="date">{formatDate(post.date)}</p>
+								<p class="description">{post.description}</p>
+							</div>
 						</a>
 					</Side_By_Side>
 				</li>
