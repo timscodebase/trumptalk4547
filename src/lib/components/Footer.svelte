3cc65008d0de5b88eb0cec8803f9a6c458a87bd0
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2024-09-27 20:12:52 -0700

    Refactor footer layout and styling

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -3,29 +3,61 @@
 </script>
 
 <footer>
-	<p>
-		&copy; {new Date().getFullYear()}
-		<span class="logo-span header-font">
-			<span class="red">Trump</span>
-			<span class="op-shadow">Talk</span>
-			<span class="blue">45/47</span>
-		</span>
-	</p>
+	<span class="logo-span header-font">
+		<span class="red">Trump</span>
+		<span class="op-shadow">Talk</span>
+		<span class="blue">45/47</span>
+	</span>
+	<p>&copy; {new Date().getFullYear()}</p>
+	<ul>
+		<li>
+			<a href="https://discord.gg/pB4DCCyC">
+				<iconify-icon icon="ic:baseline-discord"></iconify-icon>
+			</a>
+		</li>
+		<li>
+			<a href="http://www.youtube.com/@TrumpTalk4547">
+				<iconify-icon icon="mdi:youtube"></iconify-icon>
+			</a>
+		</li>
+		<li>
+			<a href="https://x.com/TrumpTalk4547">
+				<iconify-icon icon="ri:twitter-x-fill"></iconify-icon>
+			</a>
+		</li>
+		<li>
+			<a href="https://www.patreon.com/TrumpTalk4547">
+				<iconify-icon icon="fa6-brands:patreon"></iconify-icon>
+			</a>
+		</li>
+	</ul>
 </footer>
 
 <style>
 	footer {
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
 		border-top: 2px solid var(--color-accent);
 		margin-top: 2rem;
 		padding: 2rem 1rem;
 		text-align: center;
-		font-size: 1.2rem;
-	}
 
-	p {
-		width: 100%;
-		margin: 0;
-		padding: 0;
-		margin: 0 auto;
+		p {
+			font-size: 1.5rem;
+		}
+
+		ul {
+			display: flex;
+			list-style: none;
+			margin: 0;
+			padding: 0;
+			gap: 1rem;
+
+			a {
+				color: var(--color-text);
+				font-size: 1.5rem;
+			}
+		}
 	}
 </style>
--- src/lib/components/Side_By_Side.svelte
+++ src/lib/components/Side_By_Side.svelte
@@ -12,9 +12,7 @@
 		grid-template-columns: 1fr;
 		place-items: center;
 		gap: 0;
-		margin: 1rem 0;
 		overflow: hidden;
-		/* border-radius: var(--radius-3); */
 		border: 2px solid var(--color-accent);
 	}
 </style>
