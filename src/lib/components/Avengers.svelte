b4d5e49eda45a00a3b89ef454ae7d33efc9d58be
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2024-09-28 21:53:39 -0700

    Refactor image styling in Avengers.svelte and Image.svelte

---
+++ src/lib/components/Avengers.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import Image from './Image.svelte'
+</script>
+
+<div class="avengers">
+	<Image
+		src="https://res.cloudinary.com/tithos/image/upload/f_auto,q_auto:eco/v1727583704/trumps-avengers_z2945x.png"
+		alt="Trump's Avengers"
+	/>
+</div>
