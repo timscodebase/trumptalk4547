8e989106ccb1f2938392f19ef38580145e2ba6d2
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2024-10-01 19:57:57 -0700

    Add @vercel/speed-insights dependency

---
+++ src/lib/components/Analytics.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit'
+
+	injectSpeedInsights()
+</script>
+
+<svelte:head>
+	<script
+		async
+		src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2145750142271743"
+		crossorigin="anonymous"
+	></script>
+</svelte:head>
