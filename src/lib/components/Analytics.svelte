c89aade788cfdea117928b4aaf216603e27adbf0
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2024-10-02 11:48:01 -0700

    Add Google AdSense meta tag for analytics

--- src/lib/components/Analytics.svelte
+++ src/lib/components/Analytics.svelte
@@ -5,6 +5,7 @@
 </script>
 
 <svelte:head>
+	<meta name="google-adsense-account" content="ca-pub-2145750142271743" />
 	<script
 		async
 		src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2145750142271743"
