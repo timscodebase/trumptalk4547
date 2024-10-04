e6abad048b609768a8b949001d247d3aa5dac925
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2024-10-03 19:45:39 -0700

    Refactor svelte.config.js and remove Partytown Vite plugin

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,6 @@
 	import { Analylitics, CookieConsent, Footer, Header } from '$lib'
 	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit'
 	import { inject } from '@vercel/analytics'
-	import { partytownSnippet } from '@builder.io/partytown/integration'
 
 	injectSpeedInsights()
 
@@ -26,10 +25,6 @@
 		})
 	})
 </script>
-
-<svelte:head>
-	{@html '<script>' + partytownSnippet() + '</script>'}
-</svelte:head>
 
 <Analylitics />
 
