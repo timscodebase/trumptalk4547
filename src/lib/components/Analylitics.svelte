c02187ff80b51aa0b4e761862e4aaf33fc46d42b
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2024-10-05 09:31:26 -0700

    Refactor meta tags in +page.svelte to use slugified title for twitter:url and add og:locale and og:site_name meta tags

--- src/lib/components/Analylitics.svelte
+++ src/lib/components/Analylitics.svelte
@@ -14,12 +14,14 @@
 
 <Partytown
 	script={`
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2145750142271743"
+     crossorigin="anonymous" ✂prettier:content✂="">{}</script>
 <meta name="google-adsense-account" content="ca-pub-2145750142271743" />
 	<script
 		async
 		src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2145750142271743"
 		crossorigin="anonymous"
-	 ✂prettier:content✂="Cgk=">{}</script>
+	 ✂prettier:content✂="Cgk=" ✂prettier:content✂="e30=">{}</script>
 	<script async src="https://www.googletagmanager.com/gtag/js?id=G-FZH87M6DDV">
 	</script>
 	<script>
