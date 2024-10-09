56b4a845e819e08e3806dc60043353ac6f33de38
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2024-10-09 04:34:06 -0700

    Refactor Analytics component to add responsive Google AdSense integration

--- src/lib/components/Analylitics.svelte
+++ src/lib/components/Analylitics.svelte
@@ -15,10 +15,10 @@
 <Partytown
 	script={`
 	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2145750142271743"
-     crossorigin="anonymous" ✂prettier:content✂="" ✂prettier:content✂="e30=">{}</script>
+     crossorigin="anonymous" ✂prettier:content✂="" ✂prettier:content✂="e30=" ✂prettier:content✂="e30=">{}</script>
 <meta name="google-adsense-account" content="ca-pub-2145750142271743" />
 	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2145750142271743"
-     crossorigin="anonymous" ✂prettier:content✂="">{}</script>
+     crossorigin="anonymous" ✂prettier:content✂="" ✂prettier:content✂="e30=">{}</script>
 	<script async src="https://www.googletagmanager.com/gtag/js?id=G-FZH87M6DDV">
 	</script>
 	<script>
@@ -31,5 +31,18 @@
 		gtag('js', new Date())
 		gtag('config', 'G-FZH87M6DDV')
 	</script>
+
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2145750142271743"
+     crossorigin="anonymous" ✂prettier:content✂="">{}</script>
+<!-- Test 1 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2145750142271743"
+     data-ad-slot="4387845955"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
 `}
 ></Partytown>
