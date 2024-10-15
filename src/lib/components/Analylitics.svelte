eaef3d32f1de1f8380170495ae42cec95d26ae02
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2024-10-15 07:19:36 -0700

    Update @sveltejs/kit to version 2.7.1 Update @svelte-check to version 4.0.5 Update TypeScript to version 5.7.0-beta Update typescript-eslint to version 8.9.0 Update vercel to version 37.8.0 Update vite to version 5.4.9 Update vitest to version 2.1.3 Remove Partytown component from Analylitics.svelte

--- src/lib/components/Analylitics.svelte
+++ src/lib/components/Analylitics.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import { page } from '$app/stores'
-	import Partytown from 'partytown-sveltekit/Partytown.svelte'
 
 	$effect(() => {
 		if (typeof gtag !== 'undefined') {
@@ -11,29 +10,3 @@
 		}
 	})
 </script>
-
-<Partytown
-	script={`
-	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2145750142271743"
-     crossorigin="anonymous"  ✂prettier:content✂="e30=">{}</script>
-<meta name="google-adsense-account" content="ca-pub-2145750142271743" />
-	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2145750142271743"
-     crossorigin="anonymous"  ✂prettier:content✂="e30=">{}</script>
-		 <!-- HI -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=G-FZH87M6DDV">
-	</script>
-	<script>
-		window.dataLayer = window.dataLayer || []
-
-		function gtag() {
-			dataLayer.push(arguments)
-		}
-
-		gtag('js', new Date())
-		gtag('config', 'G-FZH87M6DDV')
-	</script>
-
-	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2145750142271743"
-     crossorigin="anonymous"  ✂prettier:content✂="e30=">{}</script>
-`}
-></Partytown>
