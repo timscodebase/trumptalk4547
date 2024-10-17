0a4ebd81092b3b35447bfad029184d877c747606
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2024-10-17 08:40:21 -0700

    Refactor BuyMeACoffee component to update button style and script source

--- src/lib/components/BuyMeACoffee.svelte
+++ src/lib/components/BuyMeACoffee.svelte
@@ -1,9 +1,15 @@
-<script lang="ts">
-	import Partytown from 'partytown-sveltekit/Partytown.svelte'
-</script>
-
-<Partytown
-	script={`
-    <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="trumptalk4547" data-description="Support me on Buy me a coffee!" data-message="Even a small donation can make a big impact. Contribute to Trump Talk 45/47" data-color="#5F7FFF" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
-  `}
-></Partytown>
+<svelte:head>
+	<script
+		type="text/javascript"
+		src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js"
+		data-name="bmc-button"
+		data-slug="trumptalk4547"
+		data-color="#e00707"
+		data-emoji="🇺🇸"
+		data-font="Poppins"
+		data-text="Buy me a coffee"
+		data-outline-color="#ffffff"
+		data-font-color="#ffffff"
+		data-coffee-color="#FFDD00"
+	></script>
+</svelte:head>
