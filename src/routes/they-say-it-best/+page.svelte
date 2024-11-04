7d18a023ea798f94267c64138843178ea165ed9c
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2024-11-04 15:33:42 -0800

    Add Podcasters and YouTubers components; update 'They Say It Best' page to include new sections

--- src/routes/they-say-it-best/+page.svelte
+++ src/routes/they-say-it-best/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import * as config from '$lib/config'
+	import { Podcasters, YouTubers } from '$lib'
 </script>
 
 <svelte:head>
@@ -9,7 +10,7 @@
 <h1>They say it best!</h1>
 
 <h2>YouTubers</h2>
-<section></section>
+<YouTubers />
 
 <h2>Podcasts</h2>
-<section></section>
+<Podcasters />
