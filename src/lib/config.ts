cd569b45245fee83c7f87eab09ae2b4e789fb48b
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2024-11-05 01:19:07 -0800

    czsa

--- src/lib/config.ts
+++
@@ -1,8 +0,0 @@
-import { dev } from '$app/environment'
-
-export const url = dev ? 'http://localhost:5713' : 'https://trumptalk4547.com'
-
-export const name = 'Trump Talk 45/47'
-export const description = "News, Analysis, and Speeches for President Donald J. Trump"
-export const author = 'Tim Smith <gonad2019@gmail.com>'
-export const keywords = ['svelte', 'kit', 'sveltekit', 'site', 'template']
