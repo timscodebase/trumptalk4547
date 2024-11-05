7659c3a0baac8d379e97458968aee944141f17d4
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2024-11-04 16:02:13 -0800

    Add highlighted video to 'They Say It Best' page

--- src/routes/they-say-it-best/+page.svelte
+++ src/routes/they-say-it-best/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import * as config from '$lib/config'
 	import { Podcasters, YouTubers } from '$lib'
+
+	const highlighted_video = 'https://www.youtube.com/live/PsP_xIy7YYA?si=1hthkoYAjeSYJsfs'
 </script>
 
 <svelte:head>
@@ -8,6 +10,17 @@
 </svelte:head>
 
 <h1>They say it best!</h1>
+
+<iframe
+	width="100%"
+	height="550px"
+	src="{highlighted_video};start=420"
+	title="YouTube video player"
+	frameborder="0"
+	allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+	referrerpolicy="strict-origin-when-cross-origin"
+	allowfullscreen
+></iframe>
 
 <h2>YouTubers</h2>
 <YouTubers />
