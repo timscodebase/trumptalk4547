f2c188d5af1304f3fe73739e4cb8482211c08d59
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2024-11-19 11:29:48 -0800

    countdown

--- src/lib/components/ElectionCountDown.svelte
+++ src/lib/components/ElectionCountDown.svelte
@@ -6,7 +6,7 @@
 <section>
 	<span class="header">Countdown To Inauguration Day</span>
 	<p class="blue bold op-shadow">
-		<span class="red mono">{daysUntilFutureDate(inaugurationDay)}</span>
+		<span class="red mono">{daysUntilFutureDate(inaugurationDay)} daysß</span>
 	</p>
 </section>
 
