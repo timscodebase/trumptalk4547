9ff87e8ee17329cfcff9e130d357732cc51392a9
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2024-12-24 13:11:00 -0800

    Remove edge support for serverless functions in Svelte configuration

--- svelte.config.js
+++ svelte.config.js
@@ -31,7 +31,6 @@ const config = {
 	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
 	kit: {
 		adapter: adapter({
-			edge: true, // If you want serverless functions
       isr: {
         expiration: 60, // ISR regeneration every 60 seconds for example
         bypassToken: 'trump-talk-09-01-1981' // for on-demand revalidation
