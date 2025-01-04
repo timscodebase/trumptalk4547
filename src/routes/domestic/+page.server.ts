2821856b2cbb3786ae5a1a11037f6b76ac6847bf
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2025-01-04 11:42:34 -0800

    Refactor prerender settings by removing from old routes and adding to new ones

--- src/routes/domestic/+page.server.ts
+++ src/routes/domestic/+page.server.ts
@@ -1,6 +1,5 @@
 import type { PageServerLoad } from './$types'
 import type { Post } from '$lib/types'
-export const prerender = true
 
 export const load = (async ({ fetch }: { fetch: (input: RequestInfo, init?: RequestInit) => Promise<Response> }) => {
 	const limit = 6;
