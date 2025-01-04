b3beb385f7f24668d75c6715cf7c1dd0c52c1e18
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2025-01-04 07:02:17 -0800

    Remove debug log for posts in RSS feed handler

--- src/routes/rss.xml/+server.ts
+++ src/routes/rss.xml/+server.ts
@@ -4,7 +4,6 @@ import type { Post } from '$lib/types'
 export async function GET({ fetch }) {
 	const response = await fetch('api/all-posts')
 	const posts: Post[] = await response.json()
-	console.log("Post from RSS: ", posts)
 
 	const headers = { 'Content-Type': 'application/xml' }
 
