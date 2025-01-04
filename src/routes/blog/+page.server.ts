9aa3470071b827c83870f40383b6f6ffc1e2b71d
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2025-01-04 11:15:43 -0800

    Refactor load function in blog route to simplify parameter handling

--- src/routes/blog/+page.server.ts
+++ src/routes/blog/+page.server.ts
@@ -3,9 +3,7 @@ import type { Post } from '$lib/types'
 export const prerender = true
 
 
-export const load = (async ({ fetch, url }: { fetch: (input: RequestInfo, init?: RequestInit) => Promise<Response>, url: URL }) => {
-	const limitStr = url.searchParams.get('limit');
-	const limit = limitStr ? parseInt(limitStr) : 6; // default to 5 if 'limit' is not provided or is not a number
+export const load = (async ({ fetch }: { fetch: (input: RequestInfo, init?: RequestInit) => Promise<Response> }) => {
 	const response = await fetch(`/api/all-posts`)
 	const posts: Post[] = await response.json()
 	return { posts }
